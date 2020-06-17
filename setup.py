b2556a0e94e88aa1fefae46445cdfb33a1a80a54
Tamas Simon <tamas.simon@alphacruncher.com>
2020-06-17 16:31:17 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="nuvolos",
-    version="0.2.8",
+    version="0.2.9",
     description="The Nuvolos python library for database connectivity",
     long_description=readme(),
     url="https://github.com/nuvolos-cloud/python-connector",
